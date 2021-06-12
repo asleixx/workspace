e727a1a0cbb818c22cb7c4b766cecd44bf9a0bdf
asleixx <Efe@asleixin-pcsi>
2021-06-12 19:14:26 +0300

    za2

---
+++ src/main/java/net/mcreator/gg/item/LagyenpickaxeItem.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.gg.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.gg.GgModElements;
+
+@GgModElements.ModElement.Tag
+public class LagyenpickaxeItem extends GgModElements.ModElement {
+	@ObjectHolder("gg:lagyenpickaxe")
+	public static final Item block = null;
+	public LagyenpickaxeItem(GgModElements instance) {
+		super(instance, 4);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 3000;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 2f;
+			}
+
+			public int getHarvestLevel() {
+				return 5;
+			}
+
+			public int getEnchantability() {
+				return 20;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.fromStacks(new ItemStack(LagyenitItem.block, (int) (1)));
+			}
+		}, 1, -3.4f, new Item.Properties().group(ItemGroup.TOOLS)) {
+			@Override
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasEffect(ItemStack itemstack) {
+				return true;
+			}
+		}.setRegistryName("lagyenpickaxe"));
+	}
+}
